79945c73c0310531c5f3e270d6ad0f59327580fa
AseevArtjom <Artjom4ik2008@gmail.com>
2023-10-19 19:13:06 +0300

    Add project files.

---
+++ NewsApp/Domain/News.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsApp.Domain
+{
+    public class News : INotifyPropertyChanged
+    {
+        private string? _titel;
+        private string? _description;
+        private string? _photo;
+        private string? _date;
+
+        public News(string titel,string desc,string photo,string date)
+        {
+            Titel = titel;
+            Description = desc;
+            Photo = photo;
+            Date = date;
+        }
+
+        public string? Titel
+        {
+            get { return _titel; }
+            set 
+            {
+                _titel = value;
+                OnPropertyChanged("Titel");
+            }
+        }
+
+        public string? Description
+        {
+            get { return _description; }
+            set
+            {
+                _description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+
+        public string? Photo
+        {
+            get { return _photo; }
+            set
+            {
+                _photo = value;
+                OnPropertyChanged("Photo");
+            }
+        }
+
+        public string? Date
+        {
+            get { return _date; }
+            set
+            {
+                _date = value;
+                OnPropertyChanged("Date");
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propetyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propetyName));
+        }
+    }
+}
---
+++ NewsApp/Domain/NewsList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsApp.Domain
+{
+    public class NewsList : INotifyPropertyChanged
+    {
+        public List<News>? ListNews = null;
+        public NewsList()
+        {
+            ListNews = new List<News>();
+        }
+       
+        public void AddNews(News news)
+        {
+            ListNews?.Add(news);
+        }
+
+        public void RemoveNews(News news)
+        {
+            ListNews?.Remove(news);
+        }
+
+        public void RemoveAllNews()
+        {
+            ListNews?.Clear();
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propetyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propetyName));
+        }
+    }
+}
