3165c56e7ebdbeec711cf69902ceb4a30cf7df62
kochergakoctia12345@gmail.com <kochergakoctia12345@gmail.com>
2023-10-24 20:23:22 +0300

    Новини про спорт

--- NewsApp/App.xaml.cs
+++ NewsApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NewsApp.Domain;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,13 @@ namespace NewsApp
     /// </summary>
     public partial class App : Application
     {
+        NewsList list;
+
+        public App()
+        {
+            list = new NewsList();
+            list.AddNews(new News("123", "123", "123", "123"));
+            InitializeComponent();
+        }
     }
 }
--- NewsApp/MainWindow.xaml.cs
+++ NewsApp/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -27,6 +28,16 @@ namespace NewsApp
             list = new NewsList();
             list.AddNews(new News("NEWS","Important news","","19.10.2023"));
             InitializeComponent();
+            LVMainFootball.ItemsSource = list.ListNews;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dropDownStoryboard = FindResource("DropDownStoryboard") as Storyboard;
+            if (dropDownStoryboard != null)
+            {
+                dropDownStoryboard.Begin();
+            }
         }
     }
 }
