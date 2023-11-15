144fbf666b3cec9da79d9cbc6453a945bb6e38c7
dimavoronkov2222 <dimavoronkov2222@hotmail.com>
2023-11-15 16:18:52 +0100

    123

--- NewsApp/Pages/BasketballMain.xaml.cs
+++ NewsApp/Pages/BasketballMain.xaml.cs
@@ -1,4 +1,5 @@
-﻿using NewsApp.Navigator;
+﻿using NewsApp.Domain;
+using NewsApp.Navigator;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,11 +22,25 @@ namespace NewsApp.Pages
     /// </summary>
     public partial class BasketballMain : UserControl
     {
+        NewsList list;
+
         public BasketballMain()
         {
             InitializeComponent();
+            list = new NewsList();
+            list.AddNews(new News("Kamilla Cardoso headlines season's first women's basketball starting five of the week", "South Carolina senior center Kamilla Cardoso figured prominently in the Gamecocks’ two top-15 victories that opened the season and propelled them to the No. 1 position in the latest Associated\n Press Top 25 poll.\r\n \r\nCardoso was the Gamecocks’ top scorer and rebounder as they opened the season with a scoring margin of +33.5 in wins scored over No. 10 Notre Dame (100-71) and No. 14 Maryland (114-76).\n Cardoso averaged a double-double for the week, shooting 66.7 percent from the field.\r\n \r\nIn the season opener against Notre Dame on Nov. 6 in Paris, Cardoso turned in the program’s first 20-point, 15-rebound effort since Aliyah Boston surpassed those marks in the 2022 NCAA Women’s\n Final Four game against Louisville. Cardoso, who also blocked four shots in the game, pulled down 10 of her 15 boards in the first half, including eight in the second quarter as the Gamecocks \ndominated the period 24-8. She scored 13 of her 20 points in the third quarter, including eight in the first four minutes of the period.\r\n \r\nIn the Gamecocks’ home opener against Maryland, Cardoso was a steadying force on the inside with 12 points to keep the defense in the lane for the Gamecocks’ 3-point shooters. South Carolina (2-0)\n had seven players in double figures. The 114 points scored against the Terrapins marked the most points scored against a ranked opponent in program history (previously 104 against Arkansas on Jan.\n 18, 2021).", "https://www.ncaa.com/_flysystem/public-s3/styles/original/public-s3/images/2023-11-14/Kamilla-Cardoso.jpg?itok=Zek4PCTa", "15.11.2023"));
+            list.AddNews(new News("AP men’s basketball Top 25: Michigan State, Duke tumble after rough opening-week losses", "We’re just a week into the college basketball season and already there are two major upsets in the books — though that’s tame compared to the women’s side.\r\n\r\nBoth Michigan State and Duke lost in the opening week of the 2023-24 season, which shook up the top five significantly. James Madison pulled off a second wild win late last week, too, \nand Saint Mary’s may be in trouble already.\r\n\r\nHere’s everything you missed in Week 1 of the season, and the latest Associated Press men’s basketball poll.", "https://s.yimg.com/ny/api/res/1.2/1uMA38rqpsjc9Dbsk3RC7A--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTY0MDtjZj13ZWJw/https://s.yimg.com/os/creatr-uploaded-images/2023-11/1d206420-81e5-11ee-a4de-8c6f395b44f9", "15.11.2023"));
+            list.AddNews(new News("Warriors-Timberwolves fight: Draymond Green must be suspended for putting Rudy Gobert in a WWE chokehold", "Draymond Green is a certified hothead. Everyone knows this. He gets to pass off a lot of his antics as the almost romantic cost of a fierce competitor, but that's a copout.\n The guy seems borderline obsessed with letting the basketball world know, at every possible opportunity, just how tough he is. \r\n\r\nHe did it again Tuesday night, making a fight he had nothing to do with all about him as he put Timberwolves center Rudy Gobert into a full-on WWE chokehold and dragged him away from \nthe scene like the hero he likes to think he is. ", "https://sportshub.cbsistatic.com/i/r/2023/11/15/c76a18e4-727d-4b8f-ade4-19fa764e123e/thumbnail/770x433/d2706ddaa2c7be4674b7bc9de5948e0f/draymond-getty-1.png", "15.11.2023"));
+            list.AddNews(new News("NBA considering expansion to Canada and Mexico", "The NBA is considering expansion into Canada and Mexico. The league has already held discussions with several potential franchise owners in these countries.\r\n\r\nOne possible option is to locate the franchise in Toronto, Canada. Toronto is already home to the NHL team the Toronto Maple Leafs, and the city has a large basketball fan base.\r\n\r\nAnother possible option is to locate the franchise in Mexico City, Mexico. Mexico City is the second largest city in North America, and it also has a large basketball fan base.\r\n\r\nThe NBA has not made a final decision on expansion into Canada or Mexico. However, the league said it is considering these options seriously.", "https://images.sidearmdev.com/resize?url=https%3a%2f%2fdbukjj6eu5tsf.cloudfront.net%2fncaa.org%2fimages%2f2023%2f3%2f30%2fMBB-WBB_BallHoop.JPG&width=942&type=jpeg", "15.11.2023"));
+            list.AddNews(new News("Draymond Green was ejected from the court for choking Rudy Gobert", "Draymond Green was ejected from the court during the game between the Golden State Warriors and the Minnesota Timberwolves for using a choke on Rudy Gobert.\r\n\r\nThe incident occurred in the fourth quarter of the match when Green and Gobert collided for a rebound. Green grabbed Gobert's neck and began to choke him.\n The referee immediately sent Green off the court.\r\n\r\nAs a result of the deletion, Golden State lost the match with a score of 119-116.", "https://img.championat.com/s/735x490/news/big/i/g/drejmond-grin-byl-udalyon-s-ploschadki-za-to-chto-primenil-udushayuschij-na-rudi-gobere_17000214431544300199.jpg", "15.11.2023"));
+            list.AddNews(new News("Egor Demin is second in the Eurohopes ranking among players born in 2006", "17-year-old Russian basketball player Egor Demin took second place in the Eurohopes ranking among players born in 2006.\r\n\r\nDemin plays for the CSKA-Moscow youth team. He averaged 13.9 points, 7.1 rebounds and 1.9 assists per game this season.\r\n\r\nDemin is one of the most promising young basketball players in Russia. He is already attracting the attention of foreign clubs, including the NBA.", "https://blog.nasm.org/hubfs/Training%20Basketball%20Players-1.jpg", "15.11.2023"));
+            LVNews.ItemsSource = list.ListNews;
         }
-
+        private void WrapPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var news = (sender as StackPanel).DataContext as News;
+            NavigatorObject.Switch(new NewsScreen(news.Titel, news.Description, news.Photo, news.Date));
+        }
         private void OnGotFocusHandler(object sender, RoutedEventArgs e)
         {
             Button tb = e.Source as Button;
--- NewsApp/Pages/ReligionMain.xaml.cs
+++ NewsApp/Pages/ReligionMain.xaml.cs
@@ -1,4 +1,5 @@
-﻿using NewsApp.Navigator;
+﻿using NewsApp.Domain;
+using NewsApp.Navigator;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,9 +22,19 @@ namespace NewsApp.Pages
     /// </summary>
     public partial class ReligionMain : UserControl
     {
+        NewsList list;
         public ReligionMain()
         {
             InitializeComponent();
+            list = new NewsList();
+            list.AddNews(new News("Pope Francis urges world leaders to take action on climate change", "Pope Francis has urged world leaders to take action on climate change, calling it a \"grave threat\" to the planet and its people.\n In a speech to world leaders at the United Nations climate summit in Glasgow, Scotland,\n the Pope said that the world is facing a \"moment of crisis\" and that it is time to \"act decisively\" to protect the planet. He called for a \"radical transformation\" of the global economy,\n saying that we need to move away from fossil fuels and invest in renewable energy sources.", "https://deshika.files.wordpress.com/2007/04/1.jpg", "15.11.2023"));
+            list.AddNews(new News("Archbishop of Canterbury calls for unity in divided world", "The Archbishop of Canterbury, Justin Welby, has called for unity in a divided world. In a speech to the General Assembly of the Church of England,\n the Archbishop said that the world is facing a \"crisis of division\" and that it is time to \"come together\" and find common ground.\n He called for a \"new era of cooperation\" and said that we need to \"work together to build a better future for all\".", "https://upload.wikimedia.org/wikipedia/commons/1/12/Official_portrait_of_The_Lord_Archbishop_of_Canterbury.jpg", "15.11.2023"));
+            list.AddNews(new News("Muslim leaders condemn violence against women", "Muslim leaders have condemned violence against women, calling it \"un-Islamic\". In a statement issued by the Federation of Islamic Organisations in Europe,\n the leaders said that violence against women is \"never justified\" and that it is a \"violation of Islamic law\".\n They called on all Muslims to \"speak out against violence against women\" and to do everything they can to protect women from harm.", "https://www.newarab.com/sites/default/files/media/images/B767CF4A-4BC3-43F0-8D47-A79C9AA531E2.jpg", "15.11.2023"));
+            list.AddNews(new News("Jewish community in Germany celebrates Hanukkah", "The Jewish community in Germany is celebrating Hanukkah this year with a series of events and activities.\n Hanukkah is an eight-day Jewish holiday that celebrates the rededication of the Second Temple in Jerusalem in 164 BCE.\n The holiday is marked by the lighting of the menorah, a candelabra with nine branches.\r\n\r\n", "https://m.media-amazon.com/images/I/81xWaoC89rL._AC_SX679_.jpg", "15.11.2023"));
+            list.AddNews(new News("Dalai Lama calls for compassion and understanding in times of conflict", "The Dalai Lama has called for compassion and understanding in times of conflict. In a speech to a peace conference in India,\n the Dalai Lama said that violence is \"never the answer\" and that we need to find ways to resolve our differences through dialogue and peaceful means.\n He called for a \"global movement of compassion\" and said that we need to \"learn to live together in peace and harmony\".", "https://upload.wikimedia.org/wikipedia/commons/3/30/Dalai_Lama_in_2012_02.jpg", "15.11.2023"));
+            list.AddNews(new News("Religious leaders call for peace in Ukraine", "Religious leaders from around the world have called for peace in Ukraine and an end to the war that has been raging there since February 2022.\n In a joint statement, the leaders said that they are “deeply saddened and concerned by the ongoing conflict in Ukraine” and that they are “united in our call for an immediate cessation of hostilities”.\r\n\r\nThe statement was signed by the heads of the Catholic Church, the Orthodox Church,\n the World Council of Churches, the Lutheran World Federation, the World Jewish Congress, and the Islamic Council for North America.\n The leaders called for a “diplomatic solution” to the conflict and urged all parties involved to “lay down their weapons and engage in dialogue”.\r\n\r\n“We pray for the victims of this conflict and for all those who are suffering as a result of it,” the statement said. “We also pray for the leaders of both sides,\n that they will find the wisdom and courage to choose peace over war.”\r\n\r\nThe call for peace from religious leaders comes as the war in Ukraine enters its 11th month. The conflict has caused a humanitarian crisis in the country, with millions of people displaced from their homes.\n The war has also had a significant impact on the global economy, causing energy prices to soar and disrupting supply chains.\r\n\r\nIn addition to calling for peace, religious leaders have also been providing humanitarian aid to the victims of the war. The Catholic Church, for example, has donated millions of dollars in aid to Ukraine.\n The Orthodox Church has also been providing food, shelter, and medical care to refugees.\r\n\r\nThe call for peace from religious leaders is a powerful reminder that we must all work together to end this war and bring peace to Ukraine.", "https://www.lpj.org/cached_uploads/resize/635/576/2022/03/21/dsc-0176-jpg-1647870096.jpg?_mhk=c2e9435cf68dfd5e96", "15.11.2023"));
+            LVNews.ItemsSource = list.ListNews;
+
         }
 
         private void OnGotFocusHandler(object sender, RoutedEventArgs e)
@@ -41,7 +52,11 @@ namespace NewsApp.Pages
             tb.Background = Brushes.Black;
             tb.Foreground = Brushes.White;
         }
-
+        private void WrapPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var news = (sender as StackPanel).DataContext as News;
+            NavigatorObject.Switch(new NewsScreen(news.Titel, news.Description, news.Photo, news.Date));
+        }
         private void Button1_Click(object sender, RoutedEventArgs e)
         {
             NavigatorObject.Switch(new HomeNews());
